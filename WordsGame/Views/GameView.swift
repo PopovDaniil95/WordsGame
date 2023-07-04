bfda610a0d3d98a44fceaadfcbdf672335c5ac6f
Daniil Popov <zikfaer12@gmail.com>
2023-07-04 07:48:49 +0700

    added start view

--- WordsGame/Views/GameView.swift
+++ WordsGame/Views/GameView.swift
@@ -8,9 +8,110 @@
 import SwiftUI
 
 struct GameView: View {
+    
+    @State private var word = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+        
+        VStack(spacing: 16) {
+            HStack {
+                Button {
+                    print("Quit")
+                } label: {
+                    Text("Выход")
+                        .padding(6)
+                        .padding(.horizontal)
+                        .background(Color("Mettal"))
+                        .cornerRadius(12)
+                        .padding(6)
+                        .foregroundColor(.white)
+                        .font(.custom("AvenirNext-Bold",
+                                      size: 18))
+            }
+                Spacer()
+            }
+            
+            Text("Магнитотеропия")
+                .font(.custom("AvenirNext-Bold",
+                              size: 36))
+                .foregroundColor(.white)
+
+            HStack(spacing: 12) {
+                
+                VStack {
+                    
+                    Text("0")
+                        .font(.custom("AvenirNext-Bold",
+                                      size: 60))
+                        .foregroundColor(.white)
+                    
+                    Text("Вася")
+                        .font(.custom("AvenirNext-Bold",
+                                      size: 24))
+                        .foregroundColor(.white)
+                    
+                }.padding(20)
+                    .frame(width: screen.width / 2.2,
+                           height: screen.width / 2.2)
+                    .background(Color("FirstPlayer"))
+                    .cornerRadius(26)
+                    .shadow(color: .red,
+                            radius: 4,
+                            x: 0,
+                            y: 0)
+                
+                VStack {
+                    
+                    Text("0")
+                        .font(.custom("AvenirNext-Bold",
+                                      size: 60))
+                        .foregroundColor(.white)
+                    
+                    Text("Петя")
+                        .font(.custom("AvenirNext-Bold",
+                                      size: 24))
+                        .foregroundColor(.white)
+                    
+                }.padding(20)
+                    .frame(width: screen.width / 2.2,
+                           height: screen.width / 2.2)
+                    .background(Color("SecondColor"))
+                    .cornerRadius(26)
+                    .shadow(color: .purple,
+                            radius: 4,
+                            x: 0,
+                            y: 0)
+                
+            }
+            
+            WordsTextField(word: $word,
+                           placeholder: "Ваше слово...")
+            .padding(.horizontal)
+            
+            Button {
+                print("Ready")
+                self.word = "" 
+            } label: {
+                Text("Готово!")
+                    .padding(12)
+                    .foregroundColor(.white)
+                    .frame(maxWidth: .infinity)
+                    .background(Color("Mettal"))
+                    .cornerRadius(12)
+                    .font(.custom("AvenirNext-Bold", size: 26))
+                    .padding(.horizontal)
+            }
+
+            List {
+                
+            }.listStyle(.plain)
+                .frame(maxWidth: .infinity,
+                        maxHeight: .infinity)
+            
+        }.background(Image("background"))
+        
+     }
+    
 }
 
 struct GameView_Previews: PreviewProvider {
